f5d1a7243e9e55e254078da76fd68c484016557c
DuvanR <duvanropero2@gmail.com>
2025-02-20 16:58:47 -0500

    validacion espacios vacios
    creacion de nuevo panel para credenciales vacias

--- Assets/SCRIPTS/cambiarnuevo.cs
+++ Assets/SCRIPTS/cambiarnuevo.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class cambiarnuevo : MonoBehaviour
 {
 
     [SerializeField] private GameObject m_loginUI = null;
     [SerializeField] private GameObject m_registroUI = null;
+    [SerializeField] private GameObject m_errorUI = null;
+
+
+    public TMP_InputField inputemail; 
+    public TMP_InputField inputpassword;
+    public TMP_Text txtnotificacion;
+
+
 
     // funcion ver login 
     public void showlogin()
@@ -23,4 +34,32 @@ public class cambiarnuevo : MonoBehaviour
 
 
     }
+
+   public void loginUser()
+    {
+        if(string.IsNullOrEmpty(inputemail.text)&& string.IsNullOrEmpty(inputpassword.text))
+        {
+            notificacionerror("Ningun campo puede quedar vacío, por favor ingrese email y contraseña");
+            return;
+
+        }
+
+    }
+
+    private void notificacionerror(string message)
+    {
+        txtnotificacion.text = "" + message;
+
+        m_errorUI.SetActive(true);
+
+    }
+
+    private void closenotificacion()
+    {
+        txtnotificacion.text = "";
+
+        m_errorUI.SetActive(false);
+    }
+
+
 }
