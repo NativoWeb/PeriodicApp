3dbec61e40374d3cb922ccbced0fd9822fa1e5d6
DuvanR <duvanropero2@gmail.com>
2025-02-20 15:38:01 -0500

    login-registro
    Creación de una scena con el login y el registro, con script (cambiarnuevo) de cambio de panel

---
+++ Assets/SCRIPTS/cambiarnuevo.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cambiarnuevo : MonoBehaviour
+{
+
+    [SerializeField] private GameObject m_loginUI = null;
+    [SerializeField] private GameObject m_registroUI = null;
+
+    // funcion ver login 
+    public void showlogin()
+    {
+        m_loginUI.SetActive(true);
+        m_registroUI.SetActive(false);
+
+    }
+    //funcion ver registro
+    public void showregistro()
+    {
+        m_registroUI.SetActive(true);
+        m_loginUI.SetActive(false);
+
+
+    }
+}
